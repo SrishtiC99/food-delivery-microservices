b494cd56b468df2d914173f7eb9d70c8b03a6556
SrishtiC99 <iamsri9631@gmail.com>
2023-07-04 12:28:19 +0530

    Kafka broker added for sending order-notification to restaurant

--- restaurant-service/src/main/java/com/srishti/restaurantservice/utils/Address.java
+++ restaurant-service/src/main/java/com/srishti/restaurantservice/utils/Address.java
@@ -1,12 +1,16 @@
 package com.srishti.restaurantservice.utils;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@AllArgsConstructor
+@NoArgsConstructor
 public class Address {
     private String address;
     private String city;
