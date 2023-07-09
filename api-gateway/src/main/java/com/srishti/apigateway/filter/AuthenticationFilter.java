65aee72a7c250bcfcdf0866ff0a311a9db341930
SrishtiC99 <iamsri9631@gmail.com>
2023-07-09 12:37:57 +0530

    authentication filter added in apigateway for authentication of all routes before forwarding to microservices

---
+++ api-gateway/src/main/java/com/srishti/apigateway/filter/AuthenticationFilter.java
@@ -0,0 +1,59 @@
+package com.srishti.apigateway.filter;
+
+import com.srishti.apigateway.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+
+    @Autowired
+    private RouteValidator validator;
+
+//    private RestTemplate template;
+    @Autowired
+    private JwtUtil jwtUtil;
+
+
+    public AuthenticationFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        System.out.println("Filter executing");
+        return ((exchange, chain) -> {
+            ServerHttpRequest request = null;
+            if (validator.isSecured.test(exchange.getRequest())) {
+                //header contains token or not
+                if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                    throw new RuntimeException("missing authorization header");
+                }
+
+                String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+                if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                    authHeader = authHeader.substring(7);
+                }
+                try {
+//                   //REST call to AUTH service
+//                   template.getForObject("http://auth-service//validate?token" + authHeader, String.class);
+                    jwtUtil.validateToken(authHeader);
+                    request = exchange.getRequest()
+                            .mutate()
+                            .header("loggedInUser", jwtUtil.extractUserName(authHeader))
+                            .build();
+                } catch (Exception e) {
+                    System.out.println("invalid access...!");
+                    throw new RuntimeException("un authorized access to application");
+                }
+            }
+            return chain.filter(exchange.mutate().request(request).build());
+        });
+    }
+
+    public static class Config {}
+}
